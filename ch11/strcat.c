dfee03bad337e0ea1952321bbc4ce651715a41a6
nogood1014 <65989563+nogood1014@users.noreply.github.com>
2020-05-27 11:00:07 +0900

    Add files via upload

---
+++ ch11/strcat.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char dest[80] = "C";
+
+	printf("%s\n", strcat(dest, " is "));
+	//printf("%d\n",stcat_s(dest,80," is "));
+	//printf("%s",dest);
+	printf("%s\n", strncat(dest, "a java", 2));
+	//printf("%d\n",strncat(dsst,80,"a proce",2));
+	//printf("%s\n",dest);
+	printf("%s\n", strcat(dest, "procedural "));
+	printf("%s\n", strcat(dest, "language."));
+
+	return 0;
+}
